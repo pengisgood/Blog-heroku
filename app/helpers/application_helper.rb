7a06fc658278c4ea9514ca358d695fe94dcf30b0
Grace Han <hanpanpan200@gmail.com>
2015-11-15 22:35:49 +0800

    fix: active tabs style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     markdown_render = Redcarpet::Markdown.new(MarkdownHelper::HTMLwithCodeRay, options.inject({}) { |memo, opt| memo[opt] = true; memo })
     markdown_render.render(text).html_safe
   end
+
+  def page_name(page_name)
+    content_for :page_name, page_name
+  end
+
+  def tab_style(tab_name, page_name)
+    return 'active' if tab_name == page_name
+  end
 end

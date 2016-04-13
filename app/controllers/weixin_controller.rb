a0bae66b657e6a1e10007dcba07576970c0cbbf8
Hongwei Peng <hongwei.peng@vodafone.com.au>
2016-04-13 22:59:52 +0800

    enable weixin server validation

---
+++ app/controllers/weixin_controller.rb
@@ -0,0 +1,18 @@
+class WeixinController < ApplicationController
+  def checkSignature
+    signature = params['signature']
+    timestamp = params['timestamp']
+    nonce = params['nonce']
+
+    token = '12a3ca455f19ce7e2e358b17'
+
+    require 'digest/sha1'
+    sha1 = Digest::SHA1.hexdigest [token, timestamp, nonce].sort!.join
+
+    if sha1 == signature
+      render json: params['echostr']
+    end
+
+    render :nothing => true, status: 403
+  end
+end
